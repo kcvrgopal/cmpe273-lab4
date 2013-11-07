e27e0e78994f3b5dce6424a80cbea245a2e200c3
kcvrgopal <mailmerajagopalan@gmail.com>
2013-11-06 23:54:07 -0800

    Updated app.js

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,4 +1,14 @@
 $(":button").click(function() {
-	var isbn = this.id;
-	alert('About to report lost on ISBN ' + isbn);
+    var isbn = this.id;
+    alert('About to report lost on ISBN ' + isbn);
+    $.ajax({
+    url: "/library/v1/books/"+isbn+"?status=lost",
+    type: 'PUT',
+    contentType: 'application/json',
+    success: function(result) {
+    	var btn = document.getElementById(isbn);
+    	btn.disabled = true;
+    	window.location.reload();
+    }
+    });
 });
